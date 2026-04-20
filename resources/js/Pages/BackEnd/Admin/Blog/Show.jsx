4fed171f9edf7b1cac7f57adcd4df5dda1043ace
user <user@192.168.0.11>
2026-04-20 16:57:54 +0300

    Changes

--- resources/js/Pages/BackEnd/Admin/Blog/Show.jsx
+++ resources/js/Pages/BackEnd/Admin/Blog/Show.jsx
@@ -98,7 +98,7 @@ export default function UpdateBlog() {
                                         <label>Съдържание на статията</label>
 
                                         <TinyMCETextEditor
-                                            initialValue={blogContent}
+                                           initialValue={blog.blog_content}
                                             onChange={setBlogContent}
                                         />
 
